f7dede69b50670892136fc1c85c0598b1faa67b2
HadRad500 <hadignasr@gmail.com>
2023-09-30 22:24:07 -0700

    reinstated authentication

--- index.js
+++ index.js
@@ -448,7 +448,7 @@ app.delete('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { se
 });
 
 //Read
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
